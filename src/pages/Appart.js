252ca4cb7f13bb5aca1d13902419ecdb2f0232ec
Gunerf <schwerdtlenicolas@gmail.com>
2023-06-29 16:28:41 +0200

    fin page appart

--- src/pages/Appart.js
+++ src/pages/Appart.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import AppartInfo from '../components/AppartInfo'
+import Footer from '../components/Footer'
 
 function Appart() {
   return (
     <>
       <Navbar />
       <AppartInfo />
+      <Footer />
     </>
   )
 }
