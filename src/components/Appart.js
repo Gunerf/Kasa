22b9ff900056aac1acad5c597208a32c98c0bfa3
Gunerf <schwerdtlenicolas@gmail.com>
2023-06-30 04:36:55 +0200

    image accueil

--- src/components/Appart.js
+++ src/components/Appart.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Appart.css'
+import data from '../data/logements.json'
 
 function Appart() {
+  console.log(data)
+  console.log(data.length)
   return (
     <Link to="/info">
       <div className="appart">
-        <h2 className="title">Titre de la location</h2>
+        <div className="container">
+          <img src="https://picsum.photos/800/400" className="pic" alt=""></img>
+          <h2 className="location-name">Titre de la location</h2>
+        </div>
       </div>
     </Link>
   )
--- src/components/Array.js
+++ src/components/Array.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../styles/Array.css'
-import data from '../data/logements.json'
 import Appart from './Appart'
 
 function Array() {
-  console.log(data)
   return (
     <div className="background">
+      <Appart />
+      <Appart />
       <Appart />
       <Appart />
       <Appart />
