d2666cc4e0765e01b867698f6c90b9c9de175bb8
Gunerf <schwerdtlenicolas@gmail.com>
2023-07-01 11:21:13 +0200

    page appart dynamique

--- src/components/Appart.js
+++ src/components/Appart.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Appart.css'
-import data from '../data/logements.json'
 
-function Appart() {
-  console.log(data)
-  console.log(data.length)
+function Appart(props) {
   return (
     <Link to="/info">
       <div className="appart">
         <div className="container">
-          <img src="https://picsum.photos/800/400" className="pic" alt=""></img>
-          <h2 className="location-name">Titre de la location</h2>
+          <img src={props.image} className="pic" alt=""></img>
+          <h2 className="location-name">{props.title}</h2>
         </div>
       </div>
     </Link>
--- src/components/Array.js
+++ src/components/Array.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import '../styles/Array.css'
 import Appart from './Appart'
+import data from '../data/logements.json'
 
 function Array() {
   return (
     <div className="background">
-      <Appart />
-      <Appart />
-      <Appart />
-      <Appart />
-      <Appart />
-      <Appart />
-      <Appart />
-      <Appart />
+      {data.map((appart) => (
+        <Appart title={appart.title} image={appart.cover} />
+      ))}
     </div>
   )
 }
